7eada15d146dd9878b93150026c6c2591fe772b0
aya-cs22 <aya786930@gmail.com>
2024-06-18 23:55:44 +0300

    Print "Hello, " without quotes, then print name

--- Data_type/A.js
+++ Data_type/A.js
@@ -2,11 +2,11 @@
 const readline = require('readline')
 // creat an instance of the radline
 const r1 = readline.createInterface({
-    input : process.stdin,
-    output : process.stdout,
+  input : process.stdin,
+  output : process.stdout,
 });
 //promote (is an empty string) the usr for input
 r1.question('', (userinput) => {
-    console.log("Hello, " + userinput);
-    r1.close()
+  console.log("Hello, " + userinput);
+  r1.close()
 });
