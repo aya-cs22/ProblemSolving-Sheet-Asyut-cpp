38c24c42cfd0112e19ea6316135681e18fe025e0
aya-cs22 <aya786930@gmail.com>
2024-10-02 15:58:46 +0300

    Difference

---
+++ Data_type/D-Difference.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+/**
+ * main - Entry point of the program
+ * Description: this program promts the user to enter four numbers
+ * and then calculates the expression (num1 * num2) - (num3 * num4)
+ * and then display the result as the "Difference"
+ * Return: Always 0 (Success)
+ */
+int  main(){
+    long long n1, n2, n3, n4;
+    cin>>n1>>n2>>n3>>n4;
+    cout<<"Difference = "<<((n1 * n2) - (n3 * n4));
+    return 0;
+}
