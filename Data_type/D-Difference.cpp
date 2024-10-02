8c15405c97b886d7bddc3a24ede76b212acfa8c9
aya-cs22 <aya786930@gmail.com>
2024-10-02 16:12:09 +0300

    Difference

--- Data_type/D-Difference.cpp
+++ Data_type/D-Difference.cpp
@@ -10,6 +10,6 @@ using namespace std;
 int  main(){
     long long n1, n2, n3, n4;
     cin>>n1>>n2>>n3>>n4;
-    cout<<"Difference = "<<((n1 * n2) - (n3 * n4));
+    cout<<"Difference = "<<(n1 * n2) - (n3 * n4);
     return 0;
 }
