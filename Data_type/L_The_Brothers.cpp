0543aa02e93db54e69bf059587255a00d0223371
aya-cs22 <aya786930@gmail.com>
2024-10-04 13:49:38 +0300

    The Brothers

--- Data_type/L_The_Brothers.cpp
+++ Data_type/L_The_Brothers.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
     string first_name1 , last_name1, first_name2 , last_name2;
-    cin >> first_name>>last_name>>first_name2>>last_name2;
+    cin >> first_name1>>last_name1>>first_name2>>last_name2;
     if(last_name1 == last_name2)
     {
         cout<<"ARE Brothers";
