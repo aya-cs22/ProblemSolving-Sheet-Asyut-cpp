a6827dc9855947ce1ad1755e9f28a1c622e9708f
aya-cs22 <aya786930@gmail.com>
2024-10-06 13:37:49 +0300

    Adding Bits

---
+++ Contest1/F_Adding_Bits.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+/**
+ * main - Entry point in this program.
+ * Description: in this program promts the user to enter two decimal numbers A
+ * and B and then Print the value after adding A
+ *and B
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+    int n, n2;
+    cin>>n>>n2;
+    cout<<(n ^ n2);
+    return 0;
+}
